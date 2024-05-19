2a5383e159bc8057177684367f506e9bca90cb17
lebedev05tmn <lebedev05tmn@gmail.com>
2024-05-20 00:09:19 +0300

    1.1 fix the base

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,10 @@
 import { Flat } from "@/types/interfaces";
 
 export const fetchData = async (page: number): Promise<{ data: Flat[] }> => {
-  const res = await fetch("http://localhost:8083/api/v1/flats?per_page=9", {});
+  const res = await fetch(
+    "https://dynamic-filter.aerokod.ru/api/v1/flats?per_page=9",
+    {}
+  );
   const data = await res.json();
   return data;
 };
